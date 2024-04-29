ab3a17ddb961ced0549b7bdc0e53c33d0d581371
M-Ereca <165157781+M-Ereca@users.noreply.github.com>
2024-04-29 14:23:56 +0200

    change all

--- script.js
+++ script.js
@@ -10,10 +10,20 @@ function getData() {
        // Traitez les données comme vous le souhaitez
        console.log('Données récupérées du fichier JSON :', data);
        /// ON ECRIT LE CODE ICI ! 
-      
+      let titreJournal = data.journal.nomJournal
+      console.log(titreJournal);
+      let TitrePhraseAccroche = data.journal.phraseAccroche
+      console.log(TitrePhraseAccroche);
+      let texteAppelAction = data.journal.texteAppelAction
+      console.log(texteAppelAction);
+      let articlePrincipal = data.journal.articlePrincipal
+      console.log(articlePrincipal);
+
        /// FIN DU CODE
      })
      .catch((error) => console.error('Erreur lors de la lecture des données :', error));
  }
  
  getData();
+
+ ///ON écrit les fonctions ici
